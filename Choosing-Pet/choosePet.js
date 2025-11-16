47a1900f692aa6f9dc62aa723e9d46249a1d9a60
Trisha Lohia <lohiakhushi30@gmail.com>
2025-11-16 12:50:34 +0530

    modified

--- Choosing-Pet/choosePet.js
+++ Choosing-Pet/choosePet.js
@@ -1,12 +1,5 @@
-const backBtn = document.querySelector("#back");
-const nextBtn = document.querySelector("#next");
 const saveBtn = document.querySelector("#save");
-
-// const foxBtn = document.querySelector("#baby-fox");
-// const puppyBtn = document.querySelector("#puppy");
-// const catBtn = document.querySelector("#kitty");
 const petChoices = document.querySelectorAll(".pet");
-
 let nameInput = document.querySelector("input");
 
 class petInfo{
@@ -54,14 +47,6 @@ function updatePetInfo(name, type, mood) {
     pet1.mood = mood;
 }
  
-backBtn.addEventListener("click", () => {
-    window.location.href = "../Start-page/start.html";
-});
-
-nextBtn.addEventListener("click", () => {
-    window.location.href = "../Profile-setup/profile.html";
-});
-
 saveBtn.addEventListener("click", ()=> {
     updatePetInfo(petName, petType, petMood);
     console.log(pet1.petName);
